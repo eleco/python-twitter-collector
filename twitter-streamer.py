1dae8906b4b3a6e97fc71150914f8fe05da6abd6
ericl <eric.leconte@gmail.com>
2019-04-10 08:54:38 +0100

    save last tweet id

--- twitter-streamer.py
+++ twitter-streamer.py
@@ -18,13 +18,16 @@ access_secret = os.environ.get('TWITTER_ACCESS_SECRET')
 sendgrid_key = os.environ.get('SENDGRID_KEY')
 to_email= Email(os.environ.get('SENDGRID_RECIPIENT'))
 
+#on heroku
+ON_HEROKU = 'ON_HEROKU' in os.environ
+
 #constants
 hyperlink_format = '<a href="{link}">{text}</a>'
 emailing_threshold=20
 
 
 #variables
-last_id=1
+last_tweet_id= os.environ.get('LATEST_TWEET_ID')
 emails=[]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -42,22 +45,29 @@ def send_email(emails):
     if response.status_code != 202:
         return 'An error occurred: {}'.format(response.body), response.status_code
 
+
+if not last_tweet_id:
+    last_tweet_id = 1
+
 while True:
 
-    print ("Fetching tweets since_id: " + str(last_id))
+    print ("Fetching tweets since_id: " + str(last_tweet_id))
 
-    tweets = tweepy.Cursor(api.home_timeline, wait_on_rate_limit=True, tweet_mode='extended', since_id=last_id).items(100)
+    tweets = tweepy.Cursor(api.home_timeline, wait_on_rate_limit=True, tweet_mode='extended', since_id=last_tweet_id).items(100)
     for index, tweet in enumerate(tweets):    
         
-        last_id = int(tweet.id_str) if last_id < int(tweet.id_str) else last_id    
+        last_tweet_id = int(tweet.id_str) if last_tweet_id < int(tweet.id_str) else last_tweet_id    
         
         if tweet.entities['urls']:
 
             emails.append({'user':tweet.user.screen_name , 'text': tweet.full_text, 'url': tweet.entities['urls'][0]['expanded_url']})
-            print ("tweet id :" + tweet.id_str + " last_id: " + str(last_id)  + " extracted: " + str(emails[-1]) + " emails in queue: " + str(len(emails)))
+            print ("tweet id :" + tweet.id_str + " last_id: " + str(last_tweet_id)  + " extracted: " + str(emails[-1]) + " emails in queue: " + str(len(emails)))
 
         if len(emails)>=emailing_threshold:
             send_email(emails) 
             emails=[]
+
+    if ON_HEROKU:
+        os.system('heroku config:set LAST_TWEET_ID=' + str(last_tweet_id))
 
     time.sleep(60*10)
