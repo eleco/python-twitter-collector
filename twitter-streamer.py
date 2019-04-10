0d1688347b066ddbb89f814c1276744c0130f20e
ericl <eric.leconte@gmail.com>
2019-04-10 15:45:36 +0100

    fix redis url

--- twitter-streamer.py
+++ twitter-streamer.py
@@ -48,7 +48,7 @@ def send_email(emails):
 
 
 if ON_HEROKU:
-    db=redis.from_url(os.environ['REDISCLOUD_URL'])    
+    db=redis.from_url(os.environ['REDIS_URL'])    
     last_tweet_id = db.get('last_tweet_id') 
 
 while True:
